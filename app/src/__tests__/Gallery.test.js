91a0992d5d568001a136e350ac8d0b44fe7f591b
Yuval Allweil <themarquisdesheric@gmail.com>
2017-05-26 14:41:20 -0700

    Gallery snapshot created

--- app/src/__tests__/Gallery.test.js
+++ app/src/__tests__/Gallery.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import Gallery from '../Gallery';
+import Gallery from '../components/Gallery';
 import images from './_data';
 
 describe('Gallery', () => {
@@ -9,6 +9,36 @@ describe('Gallery', () => {
     const wrapper = shallow(
       <Gallery data={images} />
     );
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('renders a Gallery view for Falling Bunny', () => {
+    const wrapper = shallow(
+      <Gallery data={images} />
+    );
+
+    wrapper.find('button').at(1).simulate('click');  
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('renders a Gallery view for Marshmellow Puff', () => {
+    const wrapper = shallow(
+      <Gallery data={images} />
+    );
+
+    wrapper.find('button').at(1).simulate('click');  
+    wrapper.find('button').at(1).simulate('click');  
+    expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('renders a Gallery view for Floppy Ears', () => {
+    const wrapper = shallow(
+      <Gallery data={images} />
+    );
+
+    wrapper.find('button').at(1).simulate('click');  
+    wrapper.find('button').at(1).simulate('click');  
+    wrapper.find('button').at(1).simulate('click');  
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
