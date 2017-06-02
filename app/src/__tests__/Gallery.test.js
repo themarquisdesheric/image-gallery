a53aa512fd0b190a2adc024ca9d74c4f11526c59
Yuval Allweil <themarquisdesheric@gmail.com>
2017-06-02 12:02:38 -0700

    updated tests

--- app/src/__tests__/Gallery.test.js
+++ app/src/__tests__/Gallery.test.js
@@ -4,17 +4,19 @@ import toJSON from 'enzyme-to-json';
 import Gallery from '../components/Gallery';
 import images from './_data';
 
+function onDelete() {}
+
 describe('Gallery', () => {
   it('renders a Gallery view for Black & Tan', () => {
     const wrapper = shallow(
-      <Gallery data={images} />
+      <Gallery data={images} onDelete={onDelete}/>
     );
     expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
   it('renders a Gallery view for Falling Bunny', () => {
     const wrapper = shallow(
-      <Gallery data={images} />
+      <Gallery data={images} onDelete={onDelete}/>
     );
 
     wrapper.find('button').at(1).simulate('click');  
@@ -23,7 +25,7 @@ describe('Gallery', () => {
 
   it('renders a Gallery view for Marshmellow Puff', () => {
     const wrapper = shallow(
-      <Gallery data={images} />
+      <Gallery data={images} onDelete={onDelete}/>
     );
 
     wrapper.find('button').at(1).simulate('click');  
@@ -33,7 +35,7 @@ describe('Gallery', () => {
 
   it('renders a Gallery view for Floppy Ears', () => {
     const wrapper = shallow(
-      <Gallery data={images} />
+      <Gallery data={images} onDelete={onDelete}/>
     );
 
     wrapper.find('button').at(1).simulate('click');  
