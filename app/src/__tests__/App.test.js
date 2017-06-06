d8c3730a45ba9b76bb0c5e93f9caea0e50853919
Yuval Allweil <themarquisdesheric@gmail.com>
2017-06-05 20:28:17 -0700

    updated tests

--- app/src/__tests__/App.test.js
+++ app/src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from '../App';
+import App from '../app/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
